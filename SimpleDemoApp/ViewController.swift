90da5ec908912c155e8b8617feab1c4629f2c66e
Admin <admin@Admins-Mac-mini.local>
2018-12-09 19:25:39 +0530

    Initial Commit

---
+++ SimpleDemoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleDemoApp
+//
+//  Created by Admin on 09/12/18.
+//  Copyright © 2018 Madhav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
